ccb1868e74a00f533149e102b65e6c97a9dbc370
Arjun Dev Singla <arjundevsingla1612@gmail.com>
2024-06-16 01:18:22 +0530

    Created Products Page

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="productCard w-[15rem] transition-all cursor-pointer">
       <div className="h-[20rem] ">
         <img
           className="h-full w-full object-cover object-left-top"
-          src="https://rukminim1.flixcart.com/image/612/612/xif0q/dress/h/l/3/xxl-6101-sheetal-associates-original-imagmnemtrdhgrnc.jpeg?q=70"
+          src={product.imageUrl}
           alt=""
         />
       </div>
 
       <div className="textPart bg-white p-3">
         <div>
-          <p className="font-bold opacity-60">Sheetal Associates</p>
-          <p>Women Fit and Flare Yellow, White, Green Dress</p>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
         </div>
         <div className="flex items-center space-x-2">
-          <p className="font-semibold ">₹199</p>
-          <p className="line-through opacity-60">₹1999</p>
-          <p className="text-green-600 font-semibold">90% off</p>
+          <p className="font-semibold ">₹{product.discountedPrice}</p>
+          <p className="line-through opacity-60">₹{product.price}</p>
+          <p className="text-green-600 font-semibold">{product.discountPersent}% off</p>
         </div>
       </div>
     </div>
